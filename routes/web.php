b5223849aacba187227892295b31b8a78280f1c6
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-06-01 12:31:22 -0500

    Manejo de routes en Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome to the Page";
+});
+
+Route::get('/posts', function(){
+    return "Welcome to the Posts";
+});
+
+Route::get('/posts/create', function(){
+    return "Welcome to the Create Post";
+});
+
+Route::get('/posts/{post}', function($post){
+    return "Welcome to the Post {$post}";
 });
