2031981fb95eac3a7d7a93b19a76c82342ef4408
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-06-01 18:34:28 -0500

    Plantillas utilzando Blade

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,22 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HomePage</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
-    <div class="max-w-4xl mx-auto px-4">
-        <h1>Bienvenido a la pagina principal</h1>
-        <x-alert2 type="success" class="mb-8">
-            <x-slot name="title">
-                Alert
-            </x-slot>
-            Contenido de la alerta
-        </x-alert2>
-        <p>Hola Mundo</p>
-    </div>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Laravel 10')
+
+@push('css')
+    <style>
+        body{
+            background: #f3f3f3;
+        }
+    </style>
+@endpush
+
+@section('content')
+<div class="max-w-4xl mx-auto px-4">
+    <h1>Bienvenido a la pagina principal</h1>
+    <x-alert2 type="success" class="mb-8">
+        <x-slot name="title">
+            Alert
+        </x-slot>
+        Contenido de la alerta
+    </x-alert2>
+    <p>Hola Mundo</p>
+</div>
+@endsection
