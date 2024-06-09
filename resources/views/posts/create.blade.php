6bc78e9f1c4f10b315a112d78d5507cb43394596
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-06-09 14:45:16 -0500

    Validaciones de campos

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,22 +1,56 @@
 <x-app-layout>
     <h1>Formulario Crear Post</h1>
+
+    @if ($errors->any())
+        <div>
+            <h2>Error</h2>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>
+                        {{ $error }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{ route('posts.store') }}" method="post">
         @csrf
         <div>
             <label>Titulo:</label>
-            <input type="text" name="title" />
+            <input type="text" name="title" value="{{ old('title') }}" />
+            {{-- @error('title')
+                <p>
+                    {{ $message }}
+                </p>
+            @enderror --}}
         </div>
         <div>
             <label>Slug:</label>
-            <input type="text" name="slug" />
+            <input type="text" name="slug" value="{{ old('slug') }}" />
+            {{-- @error('slug')
+                <p>
+                    {{ $message }}
+                </p>
+            @enderror --}}
         </div>
         <div>
             <label>Contenido:</label>
-            <textarea name="content"></textarea>
+            <textarea name="content">{{ old('content') }}</textarea>
+            {{-- @error('content')
+                <p>
+                    {{ $message }}
+                </p>
+            @enderror --}}
         </div>
         <div>
             <label>Categoria:</label>
-            <input type="text" name="category" />
+            <input type="text" name="category" value="{{ old('category') }}" />
+            {{-- @error('category')
+                <p>
+                    {{ $message }}
+                </p>
+            @enderror --}}
         </div>
         <button type="submit">Crear Post</button>
     </form>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,23 +1,35 @@
 <x-app-layout>
     <h1>Editar Post</h1>
+    @if ($errors->any())
+        <div>
+            <h2>Error</h2>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>
+                        {{ $error }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{ route('posts.update', $post) }}" method="post">
         @csrf
         @method('PUT')
         <div>
             <label>Titulo:</label>
-            <input type="text" name="title" value="{{ $post->title }}" />
+            <input type="text" name="title" value="{{ old('title', $post->title) }}" />
         </div>
         <div>
             <label>Slug:</label>
-            <input type="text" name="slug" value="{{ $post->slug }}" />
+            <input type="text" name="slug" value="{{ old('slug', $post->slug) }}" />
         </div>
         <div>
             <label>Contenido:</label>
-            <textarea name="content">{{ $post->content }}</textarea>
+            <textarea name="content">{{ old('content', $post->content) }}</textarea>
         </div>
         <div>
             <label>Categoria:</label>
-            <input type="text" name="category" value="{{ $post->category }}" />
+            <input type="text" name="category" value="{{ old('category', $post->category) }}" />
         </div>
         <button type="submit">Actualizar Post</button>
     </form>
